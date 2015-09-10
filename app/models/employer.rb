4faa318927568ab1aadfbb87269b03edf49a5f07
alerios7 <alerios7@gmail.com>
2015-09-10 16:06:35 +0000

    validar que el token exista para poder crear employer

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -29,8 +29,22 @@ class Employer < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  validate :company_id_present, on: :create
   attr_accessor :token
   belongs_to :company
-
   
+  
+  
+  def set_company_id(token)
+    company = Company.find_by(token: token)
+    
+    if company.present?
+      self.company_id = company.id 
+    end
+  end
+  
+  protected
+    def company_id_present
+      errors.add(:company_id, "El token ingresado no tiene asociada ninguna empresa") unless self.company_id.present?
+    end
 end
